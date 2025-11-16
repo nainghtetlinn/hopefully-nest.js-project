d2597d2074019253277e8c61e039ed2e553ca3b0
nainghtetlinn <naing.work.it@gmail.com>
2025-11-16 23:55:39 +0630

    style: change logger middleware log for better distinguishable

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 export function logger(req: Request, res: Response, next: NextFunction) {
   console.log(
-    `${req.method} ${req.url} ${req.body ? JSON.stringify(req.body) : ''}`,
+    `[logger] ======> ${req.method} ${req.url} ${req.body ? JSON.stringify(req.body) : ''}`,
   );
   next();
 }
