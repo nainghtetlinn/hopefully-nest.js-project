9d0b041dab4631df0d5e348162205c0420d9db9c
nainghtetlinn <naing.work.it@gmail.com>
2025-11-17 01:08:08 +0630

    feat: throw custom error

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = { userId: number; email: string }>(
+    err: any,
+    user: TUser | null,
+  ): TUser {
+    if (err || !user)
+      throw new UnauthorizedException('Invalid or expired token');
+    return user;
+  }
+}
