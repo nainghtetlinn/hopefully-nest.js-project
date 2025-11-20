897134d538f73890bf7f27ac66bc9c621948d68c
nainghtetlinn <naing.work.it@gmail.com>
2025-11-20 22:09:11 +0630

    testing transform: true

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   app.use(cookieParser());
   const config = new DocumentBuilder()
     .setTitle('Nest.js testing')
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Public()
+  @Get(':id')
+  getUserDetails(@Param('id') id: number) {
+    return {
+      type: typeof id,
+      id,
+    };
+  }
 }
